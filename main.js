3359f5e1bf8b5e8f566673408c6a18823c03f6e9
º • 🌱 kai 🌱 • º <kaisprunger@gmail.com>
2024-10-13 14:56:05 -0400

    wooo

--- main.js
+++ main.js
@@ -51,26 +51,63 @@ let projects = [
 
 let experiences = [
     {
-        title: "Experience 1",
-        timeline: "Date X - Date Y",
-        description: "A huge description of Experience 1.",
-        skills: ["Skill1", "Skill2", "Skill3"],
+        title: "PRO-DEVELOPMENT COMMITTEE MEMBER",
+        organization: "⚡ IEEE at the University of Central Florida",
+        timeline: "Aug. 2024 - Present",
+        description: "• Co-led a workshop on Linkedins, instructing attendees how to create a professional and marketable Linkedin account.<br>• Created an engaging PowerPoint presentation on career fair and career readiness, enhancing IEEE members' soft skills.<br>• Developed a user-friendly ePortfolio template out of Javascript and HTML/CSS for IEEE committee members to utilize.",
+        skills: ["Leadership", "Education", "PowerPoint", "Javascript", "GitHub", "HTML", "CSS", "Figma"],
     },
     {
-        title: "Experience 2",
-        timeline: "Date X - Date Y",
-        description: "A huge description of Experience 2.",
-        skills: ["Skill1", "Skill2", "Skill3"],
+        title: "DESIGN TEAM MEMBER",
+        organization: "🐉 Knighthacks at the University of Central Florida",
+        timeline: "Sep. 2024 - Present",
+        description: "• Designed and produced professional signage for the 2024 KnightHacks hackathon, facilitating attendee navigation and enhancing event branding.",
+        skills: ["Canva", "Figma"],
+    },
+    {
+        title: "PRESIDENT",
+        organization: "💻 Girls Who Code Club (Pine View School)",
+        timeline: "Aug. 2023 - May 2024",
+        description: "• Designed and implemented a comprehensive year-long curriculum that included Python, HTML, and CSS lessons.<br>• Ran weekly spotlight features on influential women in STEM to inspire participants.<br>• Increased members' soft and technical skills through weekly lessons and projects.",
+        skills: ["Leadership", "Education", "Python", "HTML", "CSS"],
+    },
+    {
+        title: "VICE CAPTAIN",
+        organization: "🤖 FIRST Robotics Competition Team 3627 – Jungle Robotics",
+        timeline: "Aug. 2023 - May 2024",
+        description: "• Directed and founded a VEX-IQ summer camp for elementary students, designing a comprehensive curriculum on robot construction and programming, and successfully raising over $12,000.<br>• Cultivated sponsorship relationships that led to securing over $8,000 in funding<br>• Oversaw budget management and guided 60+ team members in achieving financial targets through effective resource allocation and training.<br>• Coordinated and led 11 robotics events to inspire youth to pursue STEM, engaging over 3,500 participants.<br>• Efforts directly led to regional recognition, securing the South Florida 2024 Engineering Inspiration Award that allowed the team to compete the annual FRC World Competition.",
+        skills: ["Leadership", "Education", "Management", "Fundraising", "Event-Planning", "FIRST Robotics Competition", "VEX-IQ", "Technical Writing", "Notion"],
 
     },
     {
-        title: "Experience 3",
-        timeline: "Date X - Date Y",
-        description: "A huge description of Experience 3.",
-        skills: ["Skill1", "Skill2", "Skill3"],
+        title: "GREEN TEA CATECHINS AND ANTIBIOTICS RESEARCHER",
+        organization: "🍵 AP Capstone Program (Pine View)",
+        timeline: "Aug. 2023 - May 2024",
+        description: "• Conducted a comparative study on the antimicrobial activity of U.S. green tea brands (Numi, Bigelow, and Twinings) in combination with Ampicillin against Escherichia coli<br>• Revealed antimicrobial effectiveness and emphasizing the need for further research into antibiotic-phytochemical combination therapy.<br>• Applied the Kirby-Bauer Disk Diffusion Test to measure inhibition zones, quantifying the synergistic effects of green tea catechins and antibiotics, and analyzed data using a T-Test for statistical significance.<br>• Wrote, edited, and successfully submitted to Collegeboard a 5,000+ word research paper.<br>• For AP exams and projects associated with the AP Capstone program, received a 5 in AP Research and a 4 in AP Seminar.",
+        skills: ["Agar Plates", "BACE Certification", "Culturing Bacteria", "Lab Safety", "Micropipettes", "Technical Writing", "Editing", "Research"],
+
+    },
+    {
+        title: "EDITOR-IN-CHIEF/FOUNDER",
+        organization: "🏳️‍🌈 Iridescence LGBTQ+ Literary Magazine",
+        timeline: "Aug. 2022 - May 2024",
+        description: "• Founded and managed a youth-run LGBTQ+ literary magazine, overseeing a team of 9 to publish a 30-page issue featuring over 25 submissions from queer youth, launched free of charge.<br>• Led weekly training sessions on design tools (InDesign, Marq, Canva, Lightroom), coordinated the submissions process, and ensured timely completion of tasks while managing deadlines.<br>• Designed and laid out 10 pages, authored 1 poem, captured 4 photographs, managed social media campaigns, and designed the magazine's website (www.iridescenceliterarymagazine.com).",
+        skills: ["Leadership", "Education", "Adobe InDesign", "Adobe Lightroom", "Marq", "Canva", "Writing", "Social Media", "Professional Photography", "Figma", "Copy-Editing"],
+
+    },
+    {
+        title: "EDITOR-IN-CHIEF",
+        organization: "📰 The Match School Newspaper (Pine View)",
+        timeline: "Aug. 2022 - May 2024",
+        description: "• Led a team of 22 staff members to produce and distribute four 26-page issues of a school newspaper, informing students and parents about current events.<br>• Published 17 stories, including four editorial columns, while managing layout and editing of over 70 pages across three years on the publication team.<br>• Spearheaded editorial and approval processes, resulting in the newspaper earning a Silver rating in the annual Florida Scholastic Press Association (FSPA) Newspaper Evaluation.",
+        skills: ["Leadership", "Education", "Adobe InDesign", "Adobe Lightroom", "Copy-Editing", "Canva", "Journalistic Writing", "Interviewing", "Professional Photography"],
 
     }
+    
+    
 
+
+   
 ];
 
 
@@ -114,6 +151,7 @@ function createExperienceElement(experience) {
     experienceElement.innerHTML = `
         <div class="experience_info">
             <h3 class="experience_title">${experience.title}</h3>
+            <h2 class="experience_organization">${experience.organization}</h2>
             <h2 class="experience_timeline">${experience.timeline}</h2>
             <p class="experience_description">${experience.description}</p>
             <div class="experience_skills">
